52c70590882a302be97bf00cf65ecaec69e99727
Rahul Khanal <92104751+rahulkhanal@users.noreply.github.com>
2024-05-30 13:08:45 +0545

    swagger setup and global exception middleware were created

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,44 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { baseURL } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './middlewares/errors/global.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Task management system')
+    .setDescription('API Documentation for Healthcamp')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'Enter access-token',
+        in: 'header',
+      },
+      'access-token',
+    ).build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Global Exception
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new GlobalExceptionFilter(httpAdapterHost));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
+
 bootstrap().then(() => {
   console.log(`Server is running in ${baseURL}`);
 });
