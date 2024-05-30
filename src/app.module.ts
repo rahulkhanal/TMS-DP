0586a0cecba56497aad69d1bf1e451870db99e77
Rahul Khanal <92104751+rahulkhanal@users.noreply.github.com>
2024-05-30 13:03:11 +0545

    department modules and users modules were created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentModule } from './modules/department/department.module';
+import { UsersModule } from './modules/users/users.module';
 import pgConfig from './config/pg.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(pgConfig),
+    DepartmentModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [],
